a3665670cd33a410cec8baea765477be48710747
amarnath363 <mamarnath363@gmail.com>
2021-12-10 19:21:13 +0530

    Task done

---
+++ src/org/Market.java
@@ -0,0 +1,14 @@
+package org;
+
+public class Market {
+
+	public static void main(String[] args) {
+		System.out.println(2-5);
+		System.out.println(30.07);
+		System.out.println(3/0);
+		System.out.println(1+4);
+		System.out.println(5-4);
+		
+	}
+	
+}
